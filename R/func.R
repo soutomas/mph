3a62f3a157af7d7c47e3d3ad09d437cff454113b
Tomas Sou <soutomas@gmail.com>
2025-11-01 21:29:31 +0100

    Update documentation

--- R/func.R
+++ R/func.R
@@ -3,7 +3,7 @@
 # Use      : Convenient functions for public health 
 # Author   : Tomas Sou (soutomas)
 # Created  : 2025-10-31
-# Updated  : 2025-10-31
+# Updated  : 2025-11-01
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Notes
 # - NA
@@ -16,16 +16,16 @@
 #' Odds ratio (OR):
 #' - `OR = (Odds in the exposed)/(Odds in the unexposed)`
 #'
-#' @param a_caex `<num>` Number of cases exposed.
-#' @param b_coex `<num>` Number of controls exposed.
-#' @param c_caux `<num>` Number of cases unexposed.
-#' @param d_coux `<num>` Number of controls unexposed.
+#' @param a_case_exp `<num>` Number of cases exposed.
+#' @param b_ctrl_exp `<num>` Number of controls exposed.
+#' @param c_case_uex `<num>` Number of cases unexposed.
+#' @param d_ctrl_uex `<num>` Number of controls unexposed.
 #' @returns Odds ratio. 
 #' @export
 #' @examples
 #' odds_ratio(240,160,160,240)
-odds_ratio = function(a_caex,b_coex,c_caux,d_coux){
-  out = (a_caex/b_coex)/(c_caux/d_coux)
+odds_ratio = function(a_case_exp,b_ctrl_exp,c_case_uex,d_ctrl_uex){
+  out = (a_case_exp/b_ctrl_exp)/(c_case_uex/d_ctrl_uex)
   return(out) 
 }
 
@@ -33,7 +33,7 @@ odds_ratio = function(a_caex,b_coex,c_caux,d_coux){
 #' Risk ratio (relative risk)
 #'
 #' @description
-#'   Incidence exposed (Ie) or Risk exposed (Re)):
+#'   Incidence exposed (Ie) or Risk exposed (Re):
 #'   - `Re = cases_exposed/(cases_exposed + cases_exposed)`
 #'   
 #'   Incidence unexposed (Iu) or risk unexposed (Ru):
@@ -45,20 +45,26 @@ odds_ratio = function(a_caex,b_coex,c_caux,d_coux){
 #'   Attributable risk (AR) or risk difference:
 #'   - `AR = Incidence_exposed - Incidence_unexposed` 
 #' 
-#' @param a_caex `<num>` Number of cases exposed.
-#' @param b_ncex `<num>` Number of non-cases exposed.
-#' @param c_caux `<num>` Number of cases unexposed.
-#' @param d_ncux `<num>` Number of non-cases unexposed.
-#' @returns Risk ratio (relative risk) and/or attributable risk. 
+#' @param a_case_exp `<num>` Number of cases exposed.
+#' @param b_ncas_exp `<num>` Number of non-cases exposed.
+#' @param c_case_uex `<num>` Number of cases unexposed.
+#' @param d_ncas_uex `<num>` Number of non-cases unexposed.
+#' @returns A list containing risk exposed, risk unexposed, 
+#'   risk ratio (relative risk) and attributable risk. 
 #' @export
 #' @examples
 #' risk_ratio_attr(60,140,20,180)
 #' risk_ratio_attr(60,140,20,180)
-risk_ratio_attr = function(a_caex,b_ncex,c_caux,d_ncux){
-  risk_exposed = a_caex/(a_caex+b_ncex)
-  risk_unexposed = c_caux/(c_caux+d_ncux)
+risk_ratio_attr = function(a_case_exp,b_ncas_exp,c_case_uex,d_ncas_uex){
+  risk_exposed = a_case_exp/(a_case_exp+b_ncas_exp)
+  risk_unexposed = c_case_uex/(c_case_uex+d_ncas_uex)
   rela_risk = risk_exposed/risk_unexposed
   attr_risk = risk_exposed-risk_unexposed
-  out = list(Re=risk_exposed, Ru=risk_unexposed, RR=rela_risk, AR=attr_risk) 
+  out = list(
+    Risk_exposed = risk_exposed, 
+    Risk_unexposed = risk_unexposed, 
+    Risk_ratio = rela_risk, 
+    Attributable_risk = attr_risk
+  ) 
   return(out)
 }
